ab6243b1d9fe1b6eca9a1cceb045747365451446
李冬强 <lidongqiang@DQPro.local>
2015-04-18 22:53:50 +0800

    0418

---
+++ 基础——swift/基础——swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  基础——swift
+//
+//  Created by 李冬强 on 15/4/1.
+//  Copyright (c) 2015年 ldq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
